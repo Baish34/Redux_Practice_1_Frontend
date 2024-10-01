8884cd34d831ef36523bd134589600ef6c4a3af6
Baishnawi Agrawal <145450500+Baish34@users.noreply.github.com>
2024-10-01 05:54:42 +0000

    created sync thunk to delete and update book. Created bookform file

---
+++ src/components/bookForm.jsx
@@ -0,0 +1,8 @@
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const BookForm = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate()
+    const {}
+};
--- src/features/booksSlice.js
+++ src/features/booksSlice.js
@@ -3,17 +3,45 @@ import axios from "axios";
 
 export const fetchBooks = createAsyncThunk("books/fetchBooks", async () => {
   const response = await axios.get(
-    "redux-practice-1-oyr8wds63-baishnawi-agrawals-projects-bdb05057.vercel.app/books"
+    "https://redux-practice-1-oyr8wds63-baishnawi-agrawals-projects-bdb05057.vercel.app/books"
   );
   return response.data;
 });
 
+export const fetchBookById = createAsyncThunk(
+  "books/fetchBookById",
+  async (id) => {
+    const response = await axios.get(
+      `https://redux-practice-1-oyr8wds63-baishnawi-agrawals-projects-bdb05057.vercel.app/books/${id}`
+    );
+    return response.data;
+  }
+);
+
 export const addBook = createAsyncThunk("books/addBooks", async (book) => {
   const response = await axios.post(
-    "edux-practice-1-oyr8wds63-baishnawi-agrawals-projects-bdb05057.vercel.app/books",
+    "https://redux-practice-1-oyr8wds63-baishnawi-agrawals-projects-bdb05057.vercel.app/books",
     book
   );
   return response.data;
+});
+
+export const updateBook = createAsyncThunk(
+  "books/updateBook",
+  async ({ id, ...book }) => {
+    const response = await axios.put(
+      `https://redux-practice-1-oyr8wds63-baishnawi-agrawals-projects-bdb05057.vercel.app/books/${id}`,
+      book
+    );
+    return response.data;
+  }
+);
+
+export const deleteBook = createAsyncThunk("books/deleteBook", async (id) => {
+  await axios.delete(
+    `https://redux-practice-1-oyr8wds63-baishnawi-agrawals-projects-bdb05057.vercel.app/books/${id}`
+  );
+  return id;
 });
 
 const booksSlice = createSlice({
@@ -37,8 +65,35 @@ const booksSlice = createSlice({
         state.status = "failed";
         state.error = action.error.message;
       })
+      .addCase(fetchBookById.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchBookById.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        const book = action.payload;
+        const existingbook = state.books.find((b) => b._id === book._id);
+        if (!existingbook) {
+          state.books.push(book);
+        }
+      })
+      .addCase(fetchBookById.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      })
       .addCase(addBook.fulfilled, (state, action) => {
         state.books.push(action.payload);
+      })
+      .addCase(updateBook.fulfilled, (state, action) => {
+        const updatedBook = action.payload;
+        const existingBook = state.books.find(
+          (book) => book._id === updatedBook._id
+        );
+        if (existingBook) {
+          Object.assign(existingBook, updatedBook);
+        }
+      })
+      .addCase(deleteBook.fulfilled, (state, action) => {
+        state.books = state.books.filter((book) => book._id !== action.payload);
       });
   },
 });
