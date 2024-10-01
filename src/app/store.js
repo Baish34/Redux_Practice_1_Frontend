0d948694c80015151d6b86688d34c94562c4c6d1
Baishnawi Agrawal <145450500+Baish34@users.noreply.github.com>
2024-10-01 04:38:14 +0000

    added reducer in store.js and created a bookview file

--- src/app/store.js
+++ src/app/store.js
@@ -1,1 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import booksReducer from "../features/books/bookSlice";
+
+export default configureStore({
+  reducer: {
+    books: booksReducer,
+  },
+});
